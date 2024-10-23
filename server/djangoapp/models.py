f4b5c2940d51d344798b0ecf866279cdd9defe6c
vabpf <vuonganhbinh2@gmail.com>
2024-10-23 14:31:14 +0700

    Hands-on Lab: Add Dynamic Pages

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -19,7 +19,8 @@ class CarMake(models.Model):
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    # Many-to-One relationship
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
@@ -31,10 +32,10 @@ class CarModel(models.Model):
     year = models.IntegerField(default=2023,
         validators=[
             MaxValueValidator(2023),
-            MinValueValidator(2015)
-        ])
+            MinValueValidator(2015)])
     dealer_id = models.IntegerField(default=1)    
     # Other fields as needed
 
     def __str__(self):
         return self.name  # Return the name as the string representation
+        
